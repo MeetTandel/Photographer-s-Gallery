2dbe4107f7348cb90e6c786ae878b3297fd15591
MeetTandel <meettandel47@gmail.com>
2021-03-19 21:45:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [imagesRendered, updateImagesRendered] = useState([]);
+
+  function handlePopUpButton(image) {
+    const popUpButton = document.querySelector("#container");
+    popUpButton.style.display = "flex";
+    const popUpImage = document.getElementById("popupImage");
+    popUpImage.src = image;
+  }
+  function closePopup() {
+    const popUpButton = document.querySelector("#container");
+    popUpButton.style.display = "none";
+    const popUpImage = document.getElementById("popupImage");
+    popUpImage.src = "";
+  }
+
+  //This line of code will act as a close button as well, when you click outiside the popup image.
+  var container = document.getElementById("container");
+  window.onclick = function (event) {
+    if (event.target === container) {
+      container.style.display = "none";
+    }
+  }
+
+  function renderImages(responseData) {
+    const imagesArray = responseData.map((imageObject, index) => {
+      const imageUrl = imageObject.download_url;
+      return (
+        <div key={index} className="image-div col-sm-12 col-md-4 col-lg-3 my-4">
+          <img
+            src={imageUrl}
+            alt="arrayImage"
+            width="350"
+            className="photo-image"
+            height="250"
+            onClick={() => handlePopUpButton(imageUrl)}
+          />
+          <p className="author-text">Photo By: {imageObject.author}</p>
+          <div id="container">
+            <div id="box">
+              <img id="popupImage" alt="popUpImage" />
+              <div className="parent-btn">
+              <button id="closeButton" onClick={closePopup}>x</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    updateImagesRendered(imagesArray);
+  }
+  async function getData() {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    renderImages(response.data);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title text-center">
+        <h2>Image Gallery</h2>
+      </div>
+      <div className="row">{imagesRendered}</div>
     </div>
   );
 }
 
 export default App;
+
+
